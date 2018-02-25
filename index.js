8fb3b90ba3e274afa5b7cfff4d62127344c7b902
derekmma <hi@derek.ma>
2018-02-25 18:11:38 +0800

    draw board

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+function loadBoard(){
+    alert('hi');
+    var c=document.getElementById("myCanvas");
+    var ctx=c.getContext("2d");
+    ctx.beginPath();
+    ctx.arc(100,75,50,0,2*Math.PI);
+    ctx.stroke();
+}
+
 function allowDrop(ev) {
     ev.preventDefault();
 }
